4c3437415489d1998ab80f265b6a98f924528e98
thaiCoda <thaiboy.23396@gmail.com>
2020-01-12 15:39:53 +0700

    Second commit.

---
+++ new.java
@@ -0,0 +1,6 @@
+public class New{
+	public New(){
+
+		System.out.println("New class");
+	}
+}
--- test.java
+++ test.java
@@ -1,9 +1,10 @@
 import java.util.*;
-public class Main{
+public class Main extends New{
 	public static void main(String[] args){
 		Scanner sc = new Scanner();
 		System.out.println("enter your word: ");
 		txt=sc.nextLine();
 		System.out.printf("your word is: %s",txt);
+		new New();
 	}
 }
